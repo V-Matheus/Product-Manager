4335cdff142d8381192df57d1b32d6c68a0e6f31
V-Matheus <victormatheus507@gmail.com>
2025-07-24 15:44:25 -0300

    feat: added Button Component

---
+++ frontend/src/components/Button.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+defineProps<{
+  value?: string
+  variant?: 'primary' | 'secondary'
+}>()
+</script>
+
+<template>
+  <button :class="variant === 'primary' ? 'primary' : 'secondary'" @click="$emit('click')">
+    {{ value }}
+  </button>
+</template>
+
+<style scoped>
+.primary {
+  display: flex;
+  padding: 0.625rem 1rem;
+
+  background: #248cf2;
+  border-radius: 8px;
+
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 21px;
+
+  color: #ffffff;
+}
+
+.secondary {
+  display: flex;
+  padding: 0.625rem 1rem;
+  background: #ffffff;
+  border: 2px solid #248cf2;
+  border-radius: 8px;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 21px;
+  color: #248cf2;
+}
+</style>
